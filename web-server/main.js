5112657ff04cc91939869ded9d96ec0e27a98587
Scalaptia <fernando@haroware.com>
2023-10-12 11:39:05 -0700

    Actualizar apariencia grid

--- web-server/main.js
+++ web-server/main.js
@@ -2,6 +2,7 @@ const grid = document.getElementById("grid");
 
 for (let i = 0; i < 16 * 16; i++) {
     const cell = document.createElement("div");
-    cell.classList.add("grid-item");
+    cell.classList.add("grid-cell");
+    cell.id = `cell-${i}`;
     grid.appendChild(cell);
 }
