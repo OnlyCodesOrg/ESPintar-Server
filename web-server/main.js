3a08aede82cdc4daadebc14b8a0de7cc2642394a
Scalaptia <fernando@haroware.com>
2023-10-13 00:22:35 -0700

    Color picker

--- web-server/main.js
+++ web-server/main.js
@@ -1,8 +1,44 @@
 const grid = document.getElementById("grid");
+const controls = document.getElementById("controls");
 
 for (let i = 0; i < 16 * 16; i++) {
     const cell = document.createElement("div");
     cell.classList.add("grid-cell");
     cell.id = `cell-${i}`;
     grid.appendChild(cell);
+}
+
+const colors = {
+    0: "#000000",
+    1: "#232035",
+    2: "#44293c",
+    3: "#673930",
+    4: "#df7027",
+    5: "#d8a067",
+    6: "#efc39b",
+    7: "#fbf337",
+    8: "#98e450",
+    9: "#6abf31",
+    10: "#37946e",
+    11: "#4b682e",
+    12: "#524b24",
+    13: "#323c39",
+    14: "#3e3f75",
+    15: "#316083",
+    16: "#5a6ee0",
+    17: "#639afe",
+    18: "#5ecde5",
+    19: "#cadafc",
+    20: "#ffffff",
+    21: "#9badb7",
+    22: "#857f87",
+    23: "#686a6b",
+};
+
+for (let i = 0; i < 24; i++) {
+    const colorCell = document.createElement("div");
+    colorCell.classList.add("color-cell");
+    colorCell.id = `color-${i}`;
+    colorCell.style.backgroundColor = colors[i];
+    controls.appendChild(colorCell);
 }
